54c3772031da8dc0c25dccc55ffc6aab61ec3c82
rahi-nz <nz.rahele@gmail.com>
2024-05-20 22:13:26 +0200

    fix admin insurance

--- app/controllers/admin/insurances_controller.rb
+++ app/controllers/admin/insurances_controller.rb
@@ -1,49 +1,51 @@
 # frozen_string_literal: true
 
-class Admin::InsurancesController < ApplicationController
-  def index
-    @insurances = Insurance.all
-  end
-
-  def show
-    @insurance = Insurance.find(params[:id])
-  end
-
-  def new
-    @insurance = Insurance.new
-  end
-
-  def create
-    @insurance = Insurance.new(insurance_params)
-    if @insurance.save
-      redirect_to [:insurances, @insurances], notice: t("admin.insurances.created")
-    else
-      render :new
+module Admin
+  class InsurancesController < ApplicationController
+    def index
+      @insurances = Insurance.all
     end
-  end
 
-  def edit
-    @insurance = Insurance.find(params[:id])
-  end
-
-  def destroy
-    @insurance = Insurance.find(params[:id])
-    @insurance.destroy
-    redirect_to admin_insurances_path, notice: t("admin.insurances.deleted")
-  end
-
-  def update
-    @insurance = Insurance.find(params[:id])
-    if @insurance.update(insurance_params)
-      redirect_to admin_insurances_path, notice: t("admin.insurances.updated")
-    else
-      render :edit
+    def show
+      @insurance = Insurance.find(params[:id])
     end
-  end
 
-  private
+    def new
+      @insurance = Insurance.new
+    end
 
-  def insurance_params
-    params.require(:insurance).permit(:name, :price_cents)
-  end
+    def edit
+      @insurance = Insurance.find(params[:id])
+    end
+
+    def create
+      @insurance = Insurance.new(insurance_params)
+      if @insurance.save
+        redirect_to [:insurances, @insurances], notice: t("admin.insurances.created")
+      else
+        render :new
+      end
+    end
+
+    def update
+      @insurance = Insurance.find(params[:id])
+      if @insurance.update(insurance_params)
+        redirect_to admin_insurances_path, notice: t("admin.insurances.updated")
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @insurance = Insurance.find(params[:id])
+      @insurance.destroy
+      redirect_to admin_insurances_path, notice: t("admin.insurances.deleted")
+    end
+
+    private
+
+    def insurance_params
+      params.require(:insurance).permit(:name, :price_cents)
+    end
+  end  
 end
