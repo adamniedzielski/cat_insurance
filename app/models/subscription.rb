b218d8b7715e2f7a2690c9c68d8ce26beb9ce106
rahi-nz <nz.rahele@gmail.com>
2024-05-21 18:44:49 +0200

    add validation for  starts_on

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,15 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :insurance
+
+  validates :starts_on, presence: { message: "can't be blank" }
+  validate :starts_on_cannot_be_in_the_past
+
+  private
+
+  def starts_on_cannot_be_in_the_past
+    if starts_on.present? && starts_on < Date.today
+      errors.add(:starts_on, "can't be in the past")
+    end
+  end
 end
