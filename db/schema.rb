6ad71afe2a8772e4887317f6d64a610fe650b9d3
Raheleh <31156670+rahi-nz@users.noreply.github.com>
2024-05-27 18:01:04 +0200

    add insurance validations (#16)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_16_172640) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_160924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_16_172640) do
     t.integer "price_cents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_insurances_on_name", unique: true
   end
 
   create_table "subscriptions", force: :cascade do |t|
