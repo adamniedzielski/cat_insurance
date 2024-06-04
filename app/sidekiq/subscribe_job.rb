a0fcf480f1125f931a335c9ad6b7f0af084867f4
Adam Niedzielski <adamsunday@gmail.com>
2024-06-04 18:33:51 +0200

    Send telegram message (#21)
    * Create secret env variables for development
    
    * Send Telegram message

--- app/sidekiq/subscribe_job.rb
+++ app/sidekiq/subscribe_job.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "telegram/bot"
+
 class SubscribeJob
   include Sidekiq::Job
 
@@ -7,5 +9,11 @@ class SubscribeJob
     subscription = Subscription.find(subscription_id)
     user = subscription.user
     SubscriptionMailer.with(user:).success_subscription.deliver_now
+
+    client = Telegram::Bot::Client.new(ENV.fetch("TELEGRAM_TOKEN"))
+    client.api.send_message(
+      chat_id: ENV.fetch("TELEGRAM_CHAT_ID"),
+      text: "Subscribed to #{subscription.insurance.name}!"
+    )
   end
 end
