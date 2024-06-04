9c2ef507b6f21d5e1bf65c9329045218702d0b2a
rahi-nz <nz.rahele@gmail.com>
2024-06-04 18:33:30 +0200

    make company optional for insurance

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -2,7 +2,7 @@
 
 class Insurance < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
-  belongs_to :company
+  belongs_to :company, optional: true
 
   validates :name, presence: true, uniqueness: true
   validates :price_cents, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
