b98f0cdb5340584b01c664c25d23af9037ab4dfa
rahi-nz <nz.rahele@gmail.com>
2024-06-14 23:23:41 +0200

    add unique subscription validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,6 +6,7 @@ class Subscription < ApplicationRecord
 
   validates :starts_on, presence: true
   validate :starts_on_cannot_be_in_the_past
+  validate :unique_subscription
 
   private
 
@@ -13,5 +14,11 @@ class Subscription < ApplicationRecord
     return unless starts_on.present? && starts_on < Time.zone.today
 
     errors.add(:starts_on, "can't be in the past")
+  end
+
+  def unique_subscription
+    return unless Subscription.exists?(user_id:, insurance_id:)
+
+    errors.add(:base, "You are already subscribed to this insurance")
   end
 end
