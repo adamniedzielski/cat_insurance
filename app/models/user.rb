5df633c7b4c4c0db2cb082c255bf34c2c1af55c5
Adam Niedzielski <adamsunday@gmail.com>
2024-06-17 19:46:58 +0200

    Show list of claims

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :subscriptions, dependent: :destroy
+  has_many :claims, through: :subscriptions
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     resources :claims, only: %i[new create]
   end
 
+  resources :claims, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
