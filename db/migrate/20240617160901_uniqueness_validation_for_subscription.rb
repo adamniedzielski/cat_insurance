1a4313d62fb6fb892776e7c302a12c3204bda86c
rahi-nz <nz.rahele@gmail.com>
2024-06-17 18:21:31 +0200

    fix rubocup

--- db/migrate/20240617160901_uniqueness_validation_for_subscription.rb
+++ db/migrate/20240617160901_uniqueness_validation_for_subscription.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class UniquenessValidationForSubscription < ActiveRecord::Migration[7.1]
   def change
-    add_index :subscriptions, [:user_id, :insurance_id], unique: true
+    add_index :subscriptions, %i[user_id insurance_id], unique: true
   end
 end
