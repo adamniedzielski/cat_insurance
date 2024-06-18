268cb1fccfdbefc39aff55d5f3294d020c4a4009
Adam Niedzielski <adamsunday@gmail.com>
2024-06-18 14:46:36 +0200

    Show list of claims (#33)

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ClaimsController < ApplicationController
+  def index
+    @claims = current_user.claims.includes(subscription: :insurance)
+  end
+
   def new
     @subscription = Subscription.find(params[:subscription_id])
     @claim = @subscription.claims.build
@@ -10,7 +14,7 @@ class ClaimsController < ApplicationController
     @subscription = Subscription.find(params[:subscription_id])
     @claim = @subscription.claims.build(claim_params)
     if @claim.save
-      redirect_to subscriptions_path, notice: t("subscription.create_claim")
+      redirect_to subscriptions_path, notice: t("subscriptions.create_claim")
     else
       render :new
     end
