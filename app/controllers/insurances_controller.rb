0ef5386064a8a5de8a6e33cf3058273548617a06
rahi-nz <nz.rahele@gmail.com>
2024-07-08 16:13:47 +0200

    add search for insurance by price

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -3,6 +3,8 @@
 class InsurancesController < ApplicationController
   def index
     @insurances = Insurance.all
+    @insurances = @insurances.where(price_cents: params[:min_price].to_i..) if params[:min_price].present?
+    @insurances = @insurances.where(price_cents: ..params[:max_price].to_i) if params[:max_price].present?
   end
 
   def show
